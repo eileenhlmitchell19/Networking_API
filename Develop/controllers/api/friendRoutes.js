ae5e60f573e9b13f8cc84a369823f2ee529ad592
Eileen Mitchell <eileenhlmitchell19@gmail.com>
2022-03-12 09:52:57 -0800

    adding folders and basic comments on how to approach project

---
+++ Develop/controllers/api/friendRoutes.js
@@ -0,0 +1,8 @@
+// const router = require('express').Router();
+// const userRoutes = require('./userRoutes');
+// const friendRoutes = require('./userRoutes');
+
+// router.user( '/user', userRoutes );
+// router.user( '/users/:userId/friends', friendRoutes );
+
+// module.exports = router;
