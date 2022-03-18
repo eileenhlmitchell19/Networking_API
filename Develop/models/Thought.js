0185576babe52cf5c1f5c2bac64d2f0c115730ec
Eileen Mitchell <eileenhlmitchell19@gmail.com>
2022-03-17 18:56:17 -0700

    adding schema besides a few questions I have yet to answer

--- Develop/models/Thought.js
+++ Develop/models/Thought.js
@@ -1,43 +1,43 @@
 
-// Create a new instance of the Mongoose s
-const mongoose = require('mongoose');
+// // Create a new instance of the Mongoose s
+// const mongoose = require('mongoose');
 
-// Create a new instance of the Mongoose schema to define shape of each document
-const reactionSchema = new mongoose.Schema({
-  // 
+// // Create a new instance of the Mongoose schema to define shape of each document
+// const reactionSchema = new mongoose.Schema({
+//   // 
 
-});
+// });
 
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
+// import mongoose from 'mongoose';
+// const { Schema } = mongoose;
 
-const blogSchema = new Schema({
-  title:  String, // String is shorthand for {type: String}
-  author: String,
-  body:   String,
-  comments: [{ body: String, date: Date }],
-  date: { type: Date, default: Date.now },
-  hidden: Boolean,
-  meta: {
-    votes: Number,
-    favs:  Number
-  }
-});
+// const blogSchema = new Schema({
+//   title:  String, // String is shorthand for {type: String}
+//   author: String,
+//   body:   String,
+//   comments: [{ body: String, date: Date }],
+//   date: { type: Date, default: Date.now },
+//   hidden: Boolean,
+//   meta: {
+//     votes: Number,
+//     favs:  Number
+//   }
+// });
 
-// Create a new instance of the Mongoose schema to define shape of each document
-const thoughtSchema = new mongoose.Schema({
-  // 
-  reactions: [ reactionSchema ]
-});
+// // Create a new instance of the Mongoose schema to define shape of each document
+// const thoughtSchema = new mongoose.Schema({
+//   // 
+//   reactions: [ reactionSchema ]
+// });
 
-//Using mongoose.model() to compile a model based 
-const Thought = mongoose.model('Thought', thoughtSchema);
+// //Using mongoose.model() to compile a model based 
+// const Thought = mongoose.model('Thought', thoughtSchema);
 
-// Error handler function to be called when an error occurs when trying to save a document
-const handleError = (err) => console.error(err);
+// // Error handler function to be called when an error occurs when trying to save a document
+// const handleError = (err) => console.error(err);
 
 
-module.exports = Thought;
+// module.exports = Thought;
 
 
 //-------------------------------------------------------------------------------//
@@ -47,10 +47,20 @@ module.exports = Thought;
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const reactionSchema = new mongoose.Schema({
+  // I DONT UNDERSTAND WHAT SHOULD GO IN HERE--------------------------------HELP//
+  //reactionID should have: use mongoose ObjectId data type & Default value is set to a new objectID
+  reactionId: {  },
+  reactionBody: { type: String, required: true, maxLength: 280 },
+  username: { type: String, required: true,   },
+  //I DONT UNDERSTAND GETTER METHODS----------------------------------------HELP//
+  createAt: { type: Date, default: Date.now, get: Date.now, },
+});
+
 // Create a new instance of the Mongoose schema to define shape of each document
 const thoughtSchema = new mongoose.Schema({
-  thoughtText: { type: String, required: true, maxLength: 20 },
-  createdAt: { type: Date, default: Date.now, get: Date.now,  },
+  thoughtText: { type: String, required: true, maxLength: 280 },
+  createdAt: { type: Date, default: Date.now, get: Date.now, },
   //I DONT UNDERSTAND GETTER METHODS----------------------------------------HELP//
   username: { type: String, required: true,   },
   reactions: [ reactionSchema ],
@@ -59,7 +69,7 @@ const thoughtSchema = new mongoose.Schema({
 
 
 //Using mongoose.model() to compile a model based 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('Thought', thoughtSchema);
 
 // Error handler function to be called when an error occurs when trying to save a document
 const handleError = (err) => console.error(err);
