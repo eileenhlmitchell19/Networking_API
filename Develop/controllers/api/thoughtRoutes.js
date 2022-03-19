f325bd5a5bc50f50d80eed9841cf553cfc49496e
Eileen Mitchell <eileenhlmitchell19@gmail.com>
2022-03-18 18:03:33 -0700

    adding route info to thoughts

--- Develop/controllers/api/thoughtRoutes.js
+++ Develop/controllers/api/thoughtRoutes.js
@@ -18,14 +18,64 @@ router.get('/:thought_Id', async (req, res) => {
     } catch(err) {
         res.status(500).json()
     }  
-})
+}),
 
 // POST to create a new thought (dont forget to push the created thoughts_id to the associated user's thoughts array field)
+ // create a new post
+ module.exports = {
+    // getPosts(req, res) {
+    //   Post.find()
+    //     .then((posts) => res.json(posts))
+    //     .catch((err) => res.status(500).json(err));
+    // },
+    // getSinglePost(req, res) {
+    //   Post.findOne({ _id: req.params.postId })
+    //     .then((post) =>
+    //       !post
+    //         ? res.status(404).json({ message: 'No post with that ID' })
+    //         : res.json(post)
+    //     )
+    //     .catch((err) => res.status(500).json(err));
+    // },
+    // // create a new post
+    createPost(req, res) {
+      Thought.create(req.body)
+        .then((post) => {
+          return Thought.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { Thoughts: thought._id } },
+            { new: true }
+          );
+        })
+        //IS THIS HOW YOU DO A DELETE ROUTE?? I AM SO CONFUSED ----------------HELP//
+        // deletePost(req, res) {
+        //     Post.delete(req.body)
+        //       .then((post) => {
+        //         return Thought.findOneAndDelete(
+        //           { _id: req.body.userId },
+        //           { $addToSet: { Thoughts: thought._id } },
+        //           { new: true }
+        //         );
+        //       })
+        .then((user) =>
+          !user
+            ? res
+                .status(404)
+                .json({ message: 'Post created, but found no user with that ID' })
+            : res.json('Created the post 🎉')
+        )
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    },
+  };
 
 
 // PUT to update a thought by its_id
 
-
+// pull? from set??
+//Post.Delete
 // DELETE to remove a thought by its_id
   // deleteThought({ params, body}, res) {
     //     Thought.findOneAndDelete({ _id: params.id })
