95a65ec4b4eed58e50b7d6ea2598b76e3c9d01af
Eileen Mitchell <eileenhlmitchell19@gmail.com>
2022-03-27 09:47:02 -0700

    clean up code in friendControllers

--- Develop/controllers/friendControllers.js
+++ Develop/controllers/friendControllers.js
@@ -40,37 +40,5 @@ const friendController = {
   }
   };
 
-
-
-// // GET all friends
-// router.get('/', async (req, res) => {
-//     try{
-//         const friendData = await Friend.find();
-//     } catch(err) {
-//         res.status(500).json()
-//     }
-//     });
-
-// // GET a single friend
-// app.get('/:friend_Id', async (req, res) => {
-//     try{
-//         const friendData = await User.findById(req.params.userId);
-//     } catch(err) {
-//         res.status(500).json()
-//     }  
-// })
-
-
-
-// // POST a new friend
-// app.post('/', async (req, res) => {
-//     try{
-//         const friendData = await User.updateMany();
-//     } catch(err) {
-//         res.status(500).json()
-//     }  
-// })
-
-
 //should this be router or Friend
 module.exports = friendController;
