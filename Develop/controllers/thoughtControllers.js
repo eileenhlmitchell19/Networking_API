ddc3b0c7a7448c30a3fd899c154e1ff6677800dd
Eileen Mitchell <eileenhlmitchell19@gmail.com>
2022-03-27 09:44:06 -0700

    update thoughtControllers. change joins to json. clean up code.

--- Develop/controllers/thoughtControllers.js
+++ Develop/controllers/thoughtControllers.js
@@ -10,14 +10,6 @@ const thoughtController = {
     Thought.find()
     .then((dbdata) => {
         res.json(dbdata)
-        
-
-        // const thought = Thought.find()
-        // res.send(thought)
-        // .then((dbdata) => {
-        //     res.join(dbdata)
-        //  });
-        // .catch((err) => res.status(500).json(err));
       })
       .catch((err) => res.status(500).json(err));
   },
@@ -51,12 +43,12 @@ const thoughtController = {
     },
   postThought(req, res) {
     Thought.findOneAndUpdate(req.body).then((dbdata) => {
-        res.join(dbdata);
+        res.json(dbdata);
       });
 },
   updateThought(req, res) {
     Thought.findOneAndUpdate(req.body).then((dbdata) => {
-        res.join(dbdata);
+        res.json(dbdata);
       });
 },
 deleteThought({ params, body}, res) {
@@ -70,94 +62,6 @@ deleteThought({ params, body}, res) {
   .catch(err => res.json(err));
 }
 };
-
-
-// // GET all thoughts
-// router.get('/', async (req, res) => {
-// try{
-//     const thoughtData = await Thought.find();
-// } catch(err) {
-//     res.status(500).json()
-// }
-// });
-
-// // GET a single thought
-// router.get('/:thought_Id', async (req, res) => {
-//     try{
-//         const thoughtData = await Thought.findById(req.params.thoughtId);
-//     } catch(err) {
-//         res.status(500).json()
-//     }  
-// }),
-
-// POST to create a new thought (dont forget to push the created thoughts_id to the associated user's thoughts array field)
- // create a new post
-//  module.exports = {
-//     // getPosts(req, res) {
-    //   Post.find()
-    //     .then((posts) => res.json(posts))
-    //     .catch((err) => res.status(500).json(err));
-    // },
-    // getSinglePost(req, res) {
-    //   Post.findOne({ _id: req.params.postId })
-    //     .then((post) =>
-    //       !post
-    //         ? res.status(404).json({ message: 'No post with that ID' })
-    //         : res.json(post)
-    //     )
-    //     .catch((err) => res.status(500).json(err));
-    // },
-    // // create a new post
-    // createPost(req, res) {
-    //   Thought.create(req.body)
-    //     .then((post) => {
-    //       return Thought.findOneAndUpdate(
-    //         { _id: req.body.userId },
-    //         { $addToSet: { Thoughts: thought._id } },
-    //         { new: true }
-    //       );
-    //     })
-        //IS THIS HOW YOU DO A DELETE ROUTE?? I AM SO CONFUSED ----------------HELP//
-        // deletePost(req, res) {
-        //     Post.delete(req.body)
-        //       .then((post) => {
-        //         return Thought.findOneAndDelete(
-        //           { _id: req.body.userId },
-        //           { $addToSet: { Thoughts: thought._id } },
-        //           { new: true }
-        //         );
-        //       })
-        // .then((user) =>
-        //   !user
-        //     ? res
-        //         .status(404)
-        //         .json({ message: 'Post created, but found no user with that ID' })
-        //     : res.json('Created the post 🎉')
-        // )
-        // .catch((err) => {
-        //   console.log(err);
-        //   res.status(500).json(err);
-  //       // });
-  //   },
-  // };
-
-
-// PUT to update a thought by its_id
-
-// pull? from set??
-//Post.Delete
-// DELETE to remove a thought by its_id
-  // deleteThought({ params, body}, res) {
-  //       Thought.findOneAndDelete({ _id: params.id })
-  //       .then(deletedThought => {
-  //           if (!deletedThought) {
-  //               return res.status(404).json({ message: 'No thought found with this ID!'})
-  //           }
-  //           res.json(deletedThought);
-  //       })
-  //       .catch(err => res.json(err));
-  //   }
-
 
 module.exports = thoughtController;
 //should this be router or Thought?
